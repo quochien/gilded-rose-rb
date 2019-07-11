2878be50c2f424321486bb5675f3ac10c4d8c7b9
Hien Nguyen <quochien@gmail.com>
2019-07-11 16:04:59 +0700

    rspecs and refactoring

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,50 +5,58 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      case type_of(item)
+      when 'normal'
+        update_quality_for_normal_item(item)
+      when 'aged'
+        update_quality_for_aged_item(item)
+      when 'backstage'
+        update_quality_for_backstage_item(item)
+      when 'conjured'
+        update_quality_for_conjured_item(item)
       end
     end
+  end
+
+  def type_of(item)
+    aged_items = ['Aged Brie']
+    sulfuras_items = ['Sulfuras, Hand of Ragnaros']
+    backstage_items = ['Backstage passes to a TAFKAL80ETC concert']
+    conjured_items = ['Conjured Mana Cake']
+
+    return 'aged' if aged_items.include? item.name
+    return 'sulfuras' if sulfuras_items.include? item.name
+    return 'backstage' if backstage_items.include? item.name
+    return 'conjured' if conjured_items.include? item.name
+
+    'normal'
+  end
+
+  def update_quality_for_conjured_item(item)
+    2.times { item.quality -= 1 if item.quality > 0 }
+    item.sell_in -= 1
+    2.times { item.quality -= 1 if item.quality > 0 } if item.sell_in < 0
+  end
+
+  def update_quality_for_backstage_item(item)
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.quality < 50 && item.sell_in < 11
+    item.quality += 1 if item.quality < 50 && item.sell_in < 6
+
+    item.sell_in -= 1
+    item.quality = 0 if item.sell_in < 0
+  end
+
+  def update_quality_for_normal_item(item)
+    item.quality -= 1 if item.quality > 0
+    item.sell_in -= 1
+    item.quality -= 1 if item.sell_in < 0 && item.quality > 0
+  end
+
+  def update_quality_for_aged_item(item)
+    item.quality += 1 if item.quality < 50
+    item.sell_in -= 1
+    item.quality += 1 if item.sell_in < 0 && item.quality < 50
   end
 end
 
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -5,7 +5,162 @@ describe GildedRose do
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      expect(items[0].name).to eq 'foo'
+    end
+
+    it 'The Quality of an item is never negative' do
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(0)
+    end
+
+    it 'The Quality of an item is never more than 50' do
+      items = [Item.new('Aged Brie', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(50)
+    end
+
+    context 'normal item' do
+      let(:item1) { Item.new('+5 Dexterity Vest', 10, 12) }
+      let(:items) { [item1] }
+      let(:gilded_rose) { GildedRose.new(items) }
+
+      it 'degrades quality by 1' do
+        gilded_rose.update_quality
+        expect(item1.quality).to eq(11)
+      end
+
+      it 'degrades sell_in by 1' do
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(9)
+      end
+
+      it 'Once the sell by date has passed, Quality degrades twice as fast' do
+        10.times { gilded_rose.update_quality }
+        expect(item1.quality).to eq(2)
+        expect(item1.sell_in).to eq(0)
+
+        gilded_rose.update_quality
+        expect(item1.quality).to eq(0)
+        expect(item1.sell_in).to eq(-1)
+
+        gilded_rose.update_quality
+        expect(item1.quality).to eq(0)
+        expect(item1.sell_in).to eq(-2)
+      end
+    end
+
+    context 'Aged Brie item' do
+      let(:item1) { Item.new('Aged Brie', 2, 0) }
+      let(:items) { [item1] }
+      let(:gilded_rose) { GildedRose.new(items) }
+
+      it 'increases quality' do
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(1)
+        expect(item1.quality).to eq(1)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(0)
+        expect(item1.quality).to eq(2)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(-1)
+        expect(item1.quality).to eq(4)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(-2)
+        expect(item1.quality).to eq(6)
+      end
+    end
+
+    context 'Sulfuras item' do
+      let(:item1) { Item.new('Sulfuras, Hand of Ragnaros', 0, 80) }
+      let(:items) { [item1] }
+      let(:gilded_rose) { GildedRose.new(items) }
+
+      before do
+        80.times { gilded_rose.update_quality }
+      end
+
+      it 'never has to be sold or decreases in Quality' do
+        expect(item1.quality).to eq(80)
+        expect(item1.sell_in).to eq(0)
+      end
+    end
+
+    context 'Backstage passes item' do
+      let(:item1) { Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 20) }
+      let(:items) { [item1] }
+      let(:gilded_rose) { GildedRose.new(items) }
+
+      it 'increases quality by 1 when there are more than 10 days' do
+        item1.sell_in = 12
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(11)
+        expect(item1.quality).to eq(21)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(10)
+        expect(item1.quality).to eq(22)
+      end
+
+      it 'increases quality by 2 when there are 10 days or less' do
+        item1.sell_in = 10
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(9)
+        expect(item1.quality).to eq(22)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(8)
+        expect(item1.quality).to eq(24)
+      end
+
+      it 'increases quality by 3 when there are 5 days or less' do
+        item1.sell_in = 5
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(4)
+        expect(item1.quality).to eq(23)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(3)
+        expect(item1.quality).to eq(26)
+      end
+
+      it 'drops quality to 0 after the concert' do
+        item1.sell_in = 0
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(-1)
+        expect(item1.quality).to eq(0)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(-2)
+        expect(item1.quality).to eq(0)
+      end
+    end
+
+    context 'Conjured item' do
+      let(:item1) { Item.new('Conjured Mana Cake', 2, 6) }
+      let(:items) { [item1] }
+      let(:gilded_rose) { GildedRose.new(items) }
+
+      it 'degrades in Quality twice as fast as normal items' do
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(1)
+        expect(item1.quality).to eq(4)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(0)
+        expect(item1.quality).to eq(2)
+
+        gilded_rose.update_quality
+        expect(item1.sell_in).to eq(-1)
+        expect(item1.quality).to eq(0)
+      end
     end
   end
 end
